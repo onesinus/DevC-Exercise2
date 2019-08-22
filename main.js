fd0c624c34cb1585b63870d7a86f556317393788
kodekite <onesinus231@gmail.com>
2019-08-22 21:29:00 +0700

    Udah bisa input angka, pake tombol maupun dari keyboard
    Sudah bisa nambah angka dan simple operator (tambah kurang kali bagi pangkat sama dengan) baik dari keyboard maupun dari button yang disediakan

--- main.js
+++ main.js
@@ -1,4 +1,58 @@
-document.querySelector("button").onclick = btnOnClick;
-function btnOnClick() {
-  alert("gblg");
+var hasil = document.getElementById("hasil");
+function addNumberToBox(number) {
+  hasil.textContent  = hasil.textContent + number;//hasil.text + btn.text;
+}
+
+function addByKeyboard(e){
+	var x = event.which || event.keyCode;
+		console.log(x);
+
+	if(x == 49){
+		addNumberToBox(1);
+	}else if(x == 50){
+		addNumberToBox(2);
+	}else if(x == 51){
+		addNumberToBox(3);
+	}else if(x == 52){
+		addNumberToBox(4);
+	}else if(x == 53){
+		addNumberToBox(5);
+	}else if(x == 54){
+		addNumberToBox(6);
+	}else if(x == 55){
+		addNumberToBox(7);
+	}else if(x == 56){
+		addNumberToBox(8);
+	}else if(x == 57){
+		addNumberToBox(9);
+	}else if(x == 48){
+		addNumberToBox(0);
+	}else if(x == 42){
+		addNumberToBox(" * ");
+	}else if(x == 58 || x == 47){
+		addNumberToBox(" / ");
+	}else if(x == 43){
+		addNumberToBox(" + ");
+	}else if(x == 45){
+		addNumberToBox(" - ");
+	}else if(x == 94){
+		addNumberToBox(" ^ ");
+	}else if(x == 13){
+		hitungHasil()
+	}
+}
+
+function backspace(){
+	var x = event.which || event.keyCode;
+	if(x == 8){
+		backspaceBtn();
+	}
+}
+
+function backspaceBtn(){
+	hasil.textContent = hasil.textContent.slice(0, -1);	
+}
+
+function hitungHasil(){
+	hasil.textContent = eval(hasil.textContent);
 }
